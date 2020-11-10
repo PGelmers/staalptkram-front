14e11c2a9d908cbbb73fd05d81867c6783a4c251
Erik <erik.online17@gmail.com>
2020-11-10 11:51:25 +0100

    item-for-sale component toegevoegd.

---
+++ src/app/item-for-sale/item-for-sale.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemForSaleComponent } from './item-for-sale.component';
+
+describe('ItemForSaleComponent', () => {
+  let component: ItemForSaleComponent;
+  let fixture: ComponentFixture<ItemForSaleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ItemForSaleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ItemForSaleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/item-for-sale/item-for-sale.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-for-sale',
+  templateUrl: './item-for-sale.component.html',
+  styleUrls: ['./item-for-sale.component.css']
+})
+export class ItemForSaleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
