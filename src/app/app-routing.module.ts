fb6dde6b65bcae85f36b68052da5c56c0044ed50
runebruijn <runebruijn123@gmail.com>
2020-11-17 14:30:48 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {StartScreenComponent} from './start-screen/start-screen.component';
 import {LoginComponent} from './login/login.component';
 import {NewUserComponent} from './new-user/new-user.component';
 import {PersonalComponent} from './personal/personal.component';
+import {ItemsForSaleListComponent} from './items-for-sale-list/items-for-sale-list.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/startscreen', pathMatch: 'full'},
   {path: 'startscreen', component: StartScreenComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: NewUserComponent},
-  {path: 'personal', component: PersonalComponent}
+  {path: 'personal', component: PersonalComponent},
+  {path: 'productlist', component: ItemsForSaleListComponent}
 ];
 
 @NgModule({
