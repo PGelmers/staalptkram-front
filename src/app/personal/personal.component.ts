f6060905b651d277a38585cf75a37ad61231f1e3
Erik <erik.online17@gmail.com>
2020-11-17 16:21:36 +0100

    Initiële opzet OpenStreetMap

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
+
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
   styleUrls: ['./personal.component.css']
 })
 export class PersonalComponent implements OnInit {
+  latitude = 53.20589;
+  longitude = 6.57904;
 
-  constructor() { }
+  map: any;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 12
+      })
+    });
+    this.addPoint(this.latitude, this.longitude);
+  }
+
+  setCenter() {
+    const view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.addMarker(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(12);
+  }
+
+  addPoint(lat: number, lng: number) {
+    const vectorLayer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: [new ol.Feature({
+          geometry: new ol.geom.Point(ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857')),
+        })]
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 0.5],
+          anchorXUnits: "fraction",
+          anchorYUnits: "fraction",
+          src: "assets/img/my-icon.png"
+        })
+      })
+    });
+    this.map.addLayer(vectorLayer);
+  }
 }
