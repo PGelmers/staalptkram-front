5b06d5118302f86b495e297cc216c6c7fa9947fd
Pim <p.gelmers@live.nl>
2020-11-18 11:44:59 +0100

    Added marker

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 declare var ol: any;
 
@@ -26,35 +26,21 @@ export class PersonalComponent implements OnInit {
       ],
       view: new ol.View({
         center: ol.proj.fromLonLat([this.longitude, this.latitude]),
-        zoom: 12
+        zoom: 17,
+        marker: ol.proj.fromLonLat([this.longitude, this.latitude])
       })
     });
-    this.addPoint(this.latitude, this.longitude);
-  }
 
-  setCenter() {
-    const view = this.map.getView();
-    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
-    view.addMarker(ol.proj.fromLonLat([this.longitude, this.latitude]));
-    view.setZoom(12);
-  }
-
-  addPoint(lat: number, lng: number) {
-    const vectorLayer = new ol.layer.Vector({
+    const layer = new ol.layer.Vector({
       source: new ol.source.Vector({
-        features: [new ol.Feature({
-          geometry: new ol.geom.Point(ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857')),
-        })]
-      }),
-      style: new ol.style.Style({
-        image: new ol.style.Icon({
-          anchor: [0.5, 0.5],
-          anchorXUnits: "fraction",
-          anchorYUnits: "fraction",
-          src: "assets/img/my-icon.png"
-        })
+        features: [
+          new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([this.longitude, this.latitude]))
+          })
+        ]
       })
     });
-    this.map.addLayer(vectorLayer);
+    this.map.addLayer(layer);
   }
+
 }
