c7f9794ce6b79fac3e6d92af8d21487d806dde52
Erik <erik.online17@gmail.com>
2020-11-19 16:31:20 +0100

    Kaart in eigen component. Duplicate code verwijderd.

--- src/app/item-for-sale/item-for-sale.component.ts
+++ src/app/item-for-sale/item-for-sale.component.ts
@@ -4,10 +4,7 @@ import {Location} from '@angular/common';
 
 import {ItemForSale} from '../../model/item-for-sale';
 import {ItemForSaleService} from '../../services/item-for-sale.service';
-import {User} from '../../model/user';
-import {GlobalConstants} from '../../common/global-constants';
-
-declare var ol: any;
+import {OpenstreetmapComponent} from '../openstreetmap/openstreetmap.component';
 
 @Component({
   selector: 'app-item-for-sale',
@@ -16,18 +13,7 @@ declare var ol: any;
 })
 export class ItemForSaleComponent implements OnInit {
   itemForSale: ItemForSale;
-
-  user = new User();
-
-  // TODO: aanzetten zodra login component geïntegreerd is.
-  // latitude: number;
-  // longitude: number;
-
-  // TODO: verwijderen zodra login component geïntegreerd is.
-  latitude = 53.20589;
-  longitude = 6.57904;
-
-  map: any;
+  openStreetMap = new OpenstreetmapComponent();
 
   constructor(
     private route: ActivatedRoute,
@@ -38,7 +24,8 @@ export class ItemForSaleComponent implements OnInit {
 
   ngOnInit(): void {
     this.getSingleItemForSale(Number(this.route.snapshot.paramMap.get('productid')));
-    this.initializeMap();
+    this.openStreetMap.setCoordinates(53.20589, 6.57904);
+    this.openStreetMap.initializeMap();
   }
 
   getSingleItemForSale(id: number): void {
@@ -48,57 +35,5 @@ export class ItemForSaleComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
-  }
-
-  initializeMap(): void {
-    this.user = GlobalConstants.user;
-
-    // TODO: aanzetten zodra login component geïntegreerd is.
-    // this.latitude = this.user.latitude;
-    // this.longitude = this.user.longitude;
-
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
-        zoom: 19,
-        marker: ol.proj.fromLonLat([this.longitude, this.latitude])
-      })
-    });
-    this.addMarker();
-  }
-
-  addMarker(): void {
-    const layer = new ol.layer.Vector({
-      source: new ol.source.Vector({
-        features: [
-          new ol.Feature({
-            geometry: new ol.geom.Point(ol.proj.fromLonLat([this.longitude, this.latitude]))
-          })
-        ]
-      })
-    });
-    this.map.addLayer(layer);
-
-    const container = document.getElementById('popup');
-    const content = document.getElementById('popup-content');
-
-    const overlay = new ol.Overlay({
-      element: container,
-      autoPan: true,
-      autoPanAnimation: {
-        duration: 250
-      }
-    });
-    this.map.addOverlay(overlay);
-
-    // TODO: checken of dit werkt na integratie login component.
-    content.innerHTML = '<b>' + this.user.straatnaam + '</br>' + this.user.huisnummer + '<br>' + this.user.plaatsnaam + '</b>';
-    overlay.setPosition(ol.proj.fromLonLat([this.longitude, this.latitude]));
   }
 }
